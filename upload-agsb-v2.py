c55a8f2ad4132073d54dbb18d15cf9e159f7fa56
lzsbjp <lzsb@outlook.jp>
2025-06-19 14:23:29 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
+USER_NAME = "chakan"         # 用户名
+UUID = "f5f9af06-b668-4f93-a2dd-ddba0f444499"                     # UUID，留空则自动生成
+PORT = 55520                   # Vmess端口，留空或0则自动生成
 DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+CF_TOKEN = "eyJhIjoiZjhiZDM2MjQ2NDJkNDE1YmZlNmZhNmM1ZjBlOWM1NTIiLCJ0IjoiZGFkOWI0MjUtN2E1Ni00ZGM3LWJlMjQtZGFlZmQwMjZiMzVlIiwicyI6IlpUY3dOVEJpTWpNdFpqQTVOUzAwT1RreUxXRTVaRGd0WXpVME4yRXdNV05pWkdWaiJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
